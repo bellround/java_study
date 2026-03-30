369ae0ca75bdb8a0061194f5e927b989c18fe640
bellround <rainarmy11@gmail.com>
2026-03-30 23:13:01 +0900

    java type simple study

---
+++ src/variable_and_type/normal_type/float_type.java
@@ -0,0 +1,20 @@
+public class float_type
+{
+    public static void main(String[] args)
+    {
+        //float var1 = 3.14; // 컴파일 에러
+        // float 타입은 뒤에 f혹은 F를 붙여야 한다
+        float var2 = 3.14f;
+        // 자바의 실수형은 기본적으로 double타입이다
+        double var3 = 3.14;
+
+        //정밀도
+        float var4 = 0.1234567890123456789f;
+        double var5 = 0.1234567890123456789;
+
+        System.out.println("var2: " + var2);
+        System.out.println("var3: " + var3);
+        System.out.println("var4: " + var4);
+        System.out.println("var5: " + var5);
+    }
+}
