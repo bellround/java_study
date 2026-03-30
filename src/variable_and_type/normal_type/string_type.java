c88f92cafd4942f90573b4f1d64f7ffc58c4444c
bellround <rainarmy11@gmail.com>
2026-03-30 23:00:46 +0900

    java string typr study

---
+++ src/variable_and_type/normal_type/string_type.java
@@ -0,0 +1,14 @@
+public class string_type
+{
+    public static void main(String[] arfs)
+    {
+        // string 타입은 문자가 아닌 문자열을 저장
+        // '는 문자이고 "가 문자열이다
+        String name = "홍길동"; // 올바른 코딩
+        // char name = "홍길동"; 잘못된 코딩
+        String job = "프로그래머";
+        System.out.println(name);
+        System.out.println(job);
+
+    }
+}
